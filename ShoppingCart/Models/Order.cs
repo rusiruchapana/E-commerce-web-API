1bdaacf987b8ed08fb021db67b62024030df5841
Rusiru Chapana Jayampathi <rusiruchapana@gmail.com>
2024-10-15 09:23:48 +0530

    create order detail model class.

--- ShoppingCart/Models/Order.cs
+++ ShoppingCart/Models/Order.cs
@@ -1,6 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace ShoppingCart.Models;
 
 public class Order
 {
+    [Key]
+    public int OrderId { get; set; }
     
+    [ForeignKey("User")]
+    public int UserId { get; set; }
+    
+    
+    public User User { get; set; }
+    
+    public List<OrderDetail> OrderDetails { get; set; }
 }
--- ShoppingCart/Models/OrderDetail.cs
+++ ShoppingCart/Models/OrderDetail.cs
@@ -1,6 +1,25 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace ShoppingCart.Models;
 
 public class OrderDetail
 {
+    [Key]
+    public int OrderDetailId { get; set; }
     
+    [ForeignKey("Order")]
+    public int OrderId { get; set; }
+    
+    
+    public Order Order { get; set; }
+    
+    [Required]
+    public int ProductId { get; set; }
+    
+    
+    public Product Product { get; set; }
+    
+    [Required]
+    public int Quantity { get; set; }
 }
