2de13eecb7546cd7dc8547778c12c8eb28a92895
Rusiru Chapana Jayampathi <rusiruchapana@gmail.com>
2024-10-15 09:26:57 +0530

    create payment model  class.

--- ShoppingCart/Models/Payment.cs
+++ ShoppingCart/Models/Payment.cs
@@ -1,6 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace ShoppingCart.Models;
 
 public class Payment
 {
+    [Key]
+    public int PaymentId { get; set; }
     
+    [Required]
+    public double Amount { get; set; }
+    
+    [Required]
+    public int OrderId { get; set; }
+    
+    
+    public Order Order { get; set; }
 }
--- ShoppingCart/Models/User.cs
+++ ShoppingCart/Models/User.cs
@@ -1,6 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace ShoppingCart.Models;
 
 public class User
 {
-    
+    [Key]
+    public int UserId { get; set; }
+    [Required]
+    public string UserName { get; set; }
+    public List<Order> Orders { get; set; }
 }
