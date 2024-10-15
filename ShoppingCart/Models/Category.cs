53cb984b77e28d5a0f5f8b3880efd021d4713875
Rusiru Chapana Jayampathi <rusiruchapana@gmail.com>
2024-10-15 09:18:55 +0530

    create category model class.

--- ShoppingCart/Models/Category.cs
+++ ShoppingCart/Models/Category.cs
@@ -1,6 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace ShoppingCart.Models;
 
 public class Category
 {
+    [Key]
+    public int CategoryId { get; set; }
     
+    [Required]
+    public string Name { get; set; }
+    
+    
+    public List<Product> Products { get; set; }
 }
